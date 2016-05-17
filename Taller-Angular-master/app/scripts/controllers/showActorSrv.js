940081bc10365f4d659faf46b87df535c2a76675
Escarribur <escarribur2@gmail.com>
2016-05-17 05:35:52 -0400

    pruebas mapa

--- Taller-Angular-master/app/scripts/controllers/showActorSrv.js
+++
@@ -1,7 +0,0 @@
-angular.module('angularSpa')
-    .service('showActorSrv', function($http){
-        var urlBase = 'http://localhost:8080/sakila-backend-master/actors/';
-        this.getActor = function(actorId){
-            return $http.get(urlBase+actorId);
-        };
-    });
--- Taller-Angular-master/app/scripts/controllers/usuariosSrv.js
+++
@@ -1,7 +0,0 @@
-angular.module('angularSpa')
-    .service('usuariosService', function($http){
-        var urlBase = 'http://localhost:8080/sakila-backend-master/usuarios';
-        this.getUsuarios = function(){
-            return $http.get(urlBase);
-        };
-    });
